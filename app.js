1af308dcf6906d55097ffc6b6b61e46709613855
jaaviieerx <javiertjh@gmail.com>
2021-10-08 17:49:28 +0800

    auto style tweak

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const app = express(); 
-const port = process.env.PORT || 5000; 
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // create a GET route api called api
-app.get('/api', (req, res) => {
+app.get("/api", (req, res) => {
   const data = [
-    {'id': 1, 'message': 'First message'},
-    {'id': 2, 'message': 'Second message'},
-    {'id': 3, 'message': 'Third message'}
-  ]
+    { id: 1, message: "First message" },
+    { id: 2, message: "Second message" },
+    { id: 3, message: "Third message" },
+  ];
   res.json(data);
 });
 
 // create a hello world api
-let helloWorld = '<h1>Hello world!</h1>';
-app.get('/', (req, res) => {
-  res.send(helloWorld); 
-}); 
+let helloWorld = "<h1>Hello world!</h1>";
+app.get("/", (req, res) => {
+  res.send(helloWorld);
+});
