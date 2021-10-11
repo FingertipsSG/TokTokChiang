79542ebba5b588e1eb1c27c20430f69f47d1de38
MaifengNg <maifeng26@hotmail.com>
2021-10-11 12:49:40 +0800

    Add footer

--- tcc_frontend/src/App.js
+++ tcc_frontend/src/App.js
@@ -2,27 +2,12 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { useState, useEffect } from "react";
 import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
-import Logo from "./Assets/Images/toktoklogo.webp";
 import Login from "./Components/Login";
-import SignUp from "./Components/SignUp";
 import Home from "./Components/Home";
-
-const Navbar = ({ element }) => {
-  return (
-    <nav className={"navbar navbar-sticky"}>
-      <div className="navbar--logo-holder">
-        <Link to="/">
-          <img src={Logo} alt="logo" className="navbar--logo" />
-        </Link>
-      </div>
-      <div className={"navbar--tabs"}>
-        <Link to="/login" alt="temporary">
-          Admin
-        </Link>
-      </div>
-    </nav>
-  );
-};
+import Navbar from "./Components/Navbar";
+import Card from 'react-bootstrap/Card'
+import Logo from "./Assets/Images/toktoklogo.webp";
+import Footer from "./Components/Footer";
 
 function App() {
   let fetchUrl = "/api";
@@ -49,14 +34,10 @@ function App() {
         <Navbar />
         <div className="main-container">
           <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/login">
-              <Login />
-            </Route>
+            <Home />
           </Switch>
         </div>
+        <Footer />
       </BrowserRouter>
     </div>
   );
---
+++ tcc_frontend/src/Components/Footer.js
@@ -0,0 +1,29 @@
+import './Styles.css'
+import { Card, CardGroup } from 'react-bootstrap'
+import Logo from "../Assets/Images/toktoklogo.webp";
+import {useState, useEffect} from "react";
+
+function Footer() {
+    function createFooter(productName) {
+        return (
+            <Card className="footerCard">
+                <Card.Img variant="top" src={Logo} />
+            </Card>
+        )
+    }
+
+    const [footerArray, setFooterArray] = useState([]);
+    useEffect(()=>{
+        // This is an array containing the products name
+        const tempProductArray = ['product1', 'product2', 'product3', 'product4'];
+        setFooterArray(tempProductArray.map(createFooter));
+      }, [])
+    
+    return (
+        <CardGroup className="footerContainer">
+            {footerArray}
+        </CardGroup>
+    )
+}
+
+export default Footer;
