eed6f6ce79a17b9f3d887a84024f38e7526cea6f
jaaviieerx <javiertjh@gmail.com>
2021-10-20 14:26:03 +0800

    added API to RDS for password

--- tcc_frontend/src/dbseed.js
+++ tcc_frontend/src/dbseed.js
@@ -1,12 +1,12 @@
 var mysql = require("mysql");
-var connection = mysql.createConnection({
+var con = mysql.createConnection({
   host: "toktokchiang-temp.cm1v4ooucggd.ap-southeast-1.rds.amazonaws.com",
   user: "admin",
   password: "password",
 });
 
-connection.connect(function (err) {
+con.connect(function (err) {
   if (err) throw err;
   console.log("Connected to RDS DB");
-  connection.end();
+  con.end();
 });
