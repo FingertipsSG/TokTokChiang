da1aa5d9f206d5b34e6478b3014a2a19b706c64a
XINJINGG <xinjing-02@outlook.com>
2022-05-06 16:19:49 +0800

    half done?

--- tcc_frontend/src/App.js
+++ tcc_frontend/src/App.js
@@ -1,75 +1,71 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Login from "./Components/Auth/Login";
 import Home from "./Components/Home";
-import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 import Products from "./Components/Products";
 import AdminProducts from "./Components/AdminProducts";
 import AdminLogin from "./Components/AdminLogin";
 
 function App() {
-	let [products, setProducts] = useState([]);
-	let fetchUrl = "/getProducts";
-	function fetchApi(url) {
-		fetch(url)
-			.then((response) => {
-				return response.json();
-			})
-			.then((data) => {
-				setProducts(data);
-				//console.log(products);
-			})
-			.catch(() => {
-				// Do something for an error here
-			});
-	}
+	// let [products, setProducts] = useState([]);
+	// let fetchUrl = "/getProducts";
+	// function fetchApi(url) {
+	// 	fetch(url)
+	// 		.then((response) => {
+	// 			return response.json();
+	// 		})
+	// 		.then((data) => {
+	// 			setProducts(data);
+	// 			//console.log(products);
+	// 		})
+	// 		.catch(() => {
+	// 			// Do something for an error here
+	// 		});
+	// }
 
-	useEffect(() => {
-		fetchApi(fetchUrl);
-	}, []);
+	// useEffect(() => {
+	// 	fetchApi(fetchUrl);
+	// }, []);
 
 	return (
-		<div className="App">
-			<BrowserRouter>
-				<Switch>
-					<Route exact path="/(home)?">
-						<Navbar products={products} />
-						<Home />
-						{/* <Footer /> */}
-					</Route>
-				</Switch>
-				<Switch>
-					<Route path="/(product)([0-9])+">
-						<Navbar products={products} />
-						<Products />
-						<Footer />
-					</Route>
-				</Switch>
-				<Switch>
-					<Route path="/login">
-						<Login />
-					</Route>
-				</Switch>
-				<Switch>
-					<Route path="/admin-login">
-						<AdminLogin />
-					</Route>
-				</Switch>
-				<Switch>
-					<Route path="/admin-products">
-						<AdminProducts />
-					</Route>
-				</Switch>
-				<Switch>
-					<Route path="/products">
-						<Products />
-					</Route>
-				</Switch>
-			</BrowserRouter>
-		</div>
+		<BrowserRouter>
+			<Switch>
+				<Route exact path="/(home)?">
+					{/* <Navbar /> */}
+					<Home />
+				</Route>
+			</Switch>
+			<Switch>
+				<Route path="/(product)([0-9])+">
+					{/* <Navbar /> */}
+					<Products />
+					<Footer />
+				</Route>
+			</Switch>
+			<Switch>
+				<Route path="/login">
+					<Login />
+				</Route>
+			</Switch>
+			<Switch>
+				<Route path="/admin-login">
+					<AdminLogin />
+				</Route>
+			</Switch>
+			<Switch>
+				<Route path="/admin-products">
+					<AdminProducts />
+				</Route>
+			</Switch>
+			<Switch>
+				<Route path="/products">
+					<Products />
+				</Route>
+			</Switch>
+		</BrowserRouter>
 	);
 }
 
--- tcc_frontend/src/Components/Home.js
+++ tcc_frontend/src/Components/Home.js
@@ -1,36 +1,42 @@
 import React from "react";
 import "../Components/css/home.css";
-import background from "../Assets/Images/wash-painting.png";
+// import background from "../Assets/Images/AboutUs.png";
 // import Carousel from "react-bootstrap/Carousel";
 // import { Container, Row, Col } from "reactstrap";
 // import Button from "react-bootstrap/Button";
-import { Row } from "reactstrap";
+import { Container, Row } from "reactstrap";
+import { Helmet } from "react-helmet";
+import Navbar from "../Components/Navbar";
+
 
 
 function Home() {
 	return (
-		<div className="homePage">
-			<br />
-			<div className="firstContent">
-				<Row>
-					<div className="verticalText col-lg-3">
-						LOREM 告場
-					</div>
+		<html>
+			<body className="homeBody">
+				<Navbar />
+				<div className="firstContent">
+					<Helmet>
+						<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+						<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+						<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+					</Helmet>
+					<Container>
+						<Row>
+							<div className="firstContentText">
+								<div className="header">
+									<h2 className="title1">TOK</h2>
+									<h2 className="title1">	TOK</h2>
+									<h2 className="title2">	CHIANG</h2>
+								</div>
+								<h3> Who Are We </h3>
+							</div>
+						</Row>
+					</Container>
+				</div>
 
-					<div className="firstContentText col-lg-3">
-						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-							Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s,
-							when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-						</p>
-						<p> It was popularised in the 1960s with the release of Letraset
-							sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
-							Aldus PageMaker including versions of Lorem Ipsum.
-						</p>
-					</div>
-				</Row>
-			</div>
-			<img src={background} />
-		</div>
+			</body>
+		</html>
 	);
 }
 
