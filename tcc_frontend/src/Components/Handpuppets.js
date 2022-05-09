012bb8b7eb8a2289e0733188bd09b43a75db0e71
XINJINGG <xinjing-02@outlook.com>
2022-05-09 17:58:13 +0800

    edit coming soon layout

--- tcc_frontend/src/Components/Handpuppets.js
+++ tcc_frontend/src/Components/Handpuppets.js
@@ -14,7 +14,7 @@ function Handpuppets() {
 			</head>
 			<body className="homeBody">
 				<Navbar />
-				<h1 className="comingsoon">COMING SOON!!</h1>
+				<h1 className="comingsoon">COMING SOON</h1>
 			</body>
 		</html>
 	);
