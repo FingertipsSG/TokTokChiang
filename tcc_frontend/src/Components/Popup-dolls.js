5e13de43ad75e73454f3e306a0a204e2ab9ef6af
EanEan <chowee.20@ichat.sp.edu.sg>
2022-05-09 13:02:29 +0800

    popup page

---
+++ tcc_frontend/src/Components/Popup-dolls.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "../Components/css/popUpDolls.css";
+
+function PopupDolls() {
+    alert("Development in progress");
+    return (
+
+
+        <div className="PopupDolls">
+            <h1>hello</h1>
+           
+        </div>
+
+
+
+    );
+}
+
+export default PopupDolls;
