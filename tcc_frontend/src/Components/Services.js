0b61e3c6d2417aac9b8c10705eaccb84529368e5
EanEan <chowee.20@ichat.sp.edu.sg>
2022-05-20 16:58:08 +0800

    Services Page Responsive

--- tcc_frontend/src/Components/Services.js
+++ tcc_frontend/src/Components/Services.js
@@ -11,7 +11,7 @@ import { Table } from "react-bootstrap";
 
 function Services() {
     return (
-        <html>
+        <html className="servicesHtml">
 			<head>
 				<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 				<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
@@ -20,15 +20,13 @@ function Services() {
             <body className="services-body">
                 {/* <img src={background} width="10%" className="services-png"/> */}
                 <Navbar />
-                {/* <div className="img"></div> */}
-                {/* className="services-png" */}
+                
 
-                <div className="div-float">
+                <div className="services-div">
                     <Container>
                         <Row>
-                            <Col className=""></Col>
-                            <Col  xs={9}>
-                                <div>
+                            <Col className="services-row"  xs={11} >
+                                <div className="services">
                                     <Table borderless>
                                         <thead>
                                             <tr>
