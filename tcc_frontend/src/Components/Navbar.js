f542afd12eaa6dea00e9ea95dff32abe6fccc9db
ELIBERP <elizz2beeth@gmail.com>
2022-05-31 17:23:09 +0800

    navbar css new non-sticky

--- tcc_frontend/src/Components/Navbar.js
+++ tcc_frontend/src/Components/Navbar.js
@@ -6,13 +6,13 @@ import { Helmet } from "react-helmet";
 
 function TopNavBar() {
 	return (
-		<div className="body">
+		<div>
 			<Helmet>
 				<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 				<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 				<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
 			</Helmet>
-			<nav className="navbar navbar-expand-lg navbar-light" style={{position: "fixed"}}>
+			<nav className="navbar navbar-expand-lg navbar-light">
 				<NavLink to="/" className="navbar_img">
 					<img src={Logo} alt="logo" className="navbar-brand navbar__logo" />
 				</NavLink>
