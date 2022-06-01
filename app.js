3a1dd803dbf3b582d74c311d383a55ebc7e2731f
ELIBERP <elizz2beeth@gmail.com>
2022-06-02 00:14:43 +0800

    static files

--- app.js
+++ app.js
@@ -7,5 +7,9 @@ app.get('/', (req,res) => {
     res.send('Welcome to Daily Code Buffer in Heroku Auto Deployment!!');
 })
 
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+});
+
 const port = process.env.PORT || '5000';
 app.listen(port, () => console.log(`Server started on Port ${port}`));
