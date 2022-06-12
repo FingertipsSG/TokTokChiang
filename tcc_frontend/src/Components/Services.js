769b193790cab5d45a413689f958b7b163399ae1
EanEan <chowee.20@ichat.sp.edu.sg>
2022-06-13 01:07:53 +0800

    Services CSS Responsive

--- tcc_frontend/src/Components/Services.js
+++ tcc_frontend/src/Components/Services.js
@@ -13,73 +13,77 @@ function Services() {
 				<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
 			</head>
             <body className="services-body">
-                <Navbar />
-                <div className="services-div">
-                    <Container>
-                        <Row>
-                            <Col className="services-row"  xs={11} >
-                                <div className="services">
-                                    <Table className="services-table" borderless >
-                                        <thead>
-                                            <tr>
-                                                <th>EVENT PLANNING</th>
-                                                <th>ENTERTAINMENT</th>
-                                                <th>CULTURAL ACTIVITIES</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr>
-                                                <td>Audiovisual</td>
-                                                <td>Emcees</td>
-                                                <td>Chinese Opera Performance</td>
-                                            </tr>
-                                            <tr>
-                                                <td>Catering</td>
-                                                <td>Entertainers</td>
-                                                <td>Chinese Opera Arias</td>
-                                            </tr>
-                                            <tr>
-                                                <td>Creative Consultancy</td>
-                                                <td>Cultural Dance</td>
-                                                <td>Chinese Opera Puppetry</td>
-                                            </tr>
-                                            <tr>
-                                                <td>Design & Conceptualisation</td>
-                                                <td>Musical Acts</td>
-                                                <td>Chinese Opera Showcase</td>
-                                            </tr>
-                                            <tr>
-                                                <td>Games & Activities</td>
-                                                <td>Stage Performances</td>
-                                                <td>Chinese Opera Costumes Rental</td>
-                                            </tr>
-                                            <tr>
-                                                <td>Exhibition Coordination</td>
-                                                <td>Stage Production</td>
-                                                <td>Chinese Opera Make-up Service</td>
-                                            </tr>
-                                            <tr>
-                                                <td>Film Choreography</td>
-                                                <td>Talent Management</td>
-                                                <td>Chinese Opera Workshops</td>
-                                            </tr>
-                                            <tr>
-                                                <td>Theatre Performances</td>
-                                                <td>Video Production</td>
-                                                <td>Multi-cultural Workshops</td>
-                                            </tr>
-                                            <tr>
-                                                <td></td>
-                                                <td>Theatre Performances</td>
-                                                <td>Mask Painting Workshops</td>
-                                            </tr>
-                                        </tbody>
-                                    </Table>
-                                </div>
-                            </Col>
-                        </Row>
-                    </Container>
-                </div>
+            <div className="bottomSection">
+                 <div className="leftDiv">
+                        <Navbar />
+                        <div className="services-div">
+                            <Container>
+                                <Row>
+                                    <Col className="services-row"  xs={11} >
+                                        <div className="services">
+                                            <Table className="services-table" borderless >
+                                                <thead>
+                                                    <tr>
+                                                        <th>EVENT PLANNING</th>
+                                                        <th>ENTERTAINMENT</th>
+                                                        <th>CULTURAL ACTIVITIES</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <tr>
+                                                        <td>Audiovisual</td>
+                                                        <td>Emcees</td>
+                                                        <td>Chinese Opera Performance</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Catering</td>
+                                                        <td>Entertainers</td>
+                                                        <td>Chinese Opera Arias</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Creative Consultancy</td>
+                                                        <td>Cultural Dance</td>
+                                                        <td>Chinese Opera Puppetry</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Design & Conceptualisation</td>
+                                                        <td>Musical Acts</td>
+                                                        <td>Chinese Opera Showcase</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Games & Activities</td>
+                                                        <td>Stage Performances</td>
+                                                        <td>Chinese Opera Costumes Rental</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Exhibition Coordination</td>
+                                                        <td>Stage Production</td>
+                                                        <td>Chinese Opera Make-up Service</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Film Choreography</td>
+                                                        <td>Talent Management</td>
+                                                        <td>Chinese Opera Workshops</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Theatre Performances</td>
+                                                        <td>Video Production</td>
+                                                        <td>Multi-cultural Workshops</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td></td>
+                                                        <td>Theatre Performances</td>
+                                                        <td>Mask Painting Workshops</td>
+                                                    </tr>
+                                                </tbody>
+                                            </Table>
+                                        </div>
+                                    </Col>
+                                </Row>
+                            </Container>
+                        </div>
+                 </div>
+            </div>
             </body>
         </html>
     );
